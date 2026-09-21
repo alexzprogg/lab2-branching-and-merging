ef4d2dfdecbabb9835c3b5553c57105383978610
Alex Mack-Mumford <Alexmackm88@gmail.com>
2026-09-21 16:11:44 -0400

    Finished task 1

--- test/DataTypesTest.java
+++ test/DataTypesTest.java
@@ -4,14 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DataTypesTest {
 
     @Test
-    /**
-     * Test that DataTypes.sum returns the correct value for
-     * the sum from 1 to 1 million.
-     */
+
     public void largeSumTest() {
 
         // You put an L at the end to indicate it is a long.
-        // try removing the L and you'll see that you get an error
+        // try removing the L, and you'll see that you get an error
         // saying "Integer number too large".
         // Note: You can use _ to help make it easier to
         //       read large numbers, as is done below.
